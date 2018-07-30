745ed6d4291bf41f11ae97efb61a81dfaa211339
vemsani123priya <41038807+vemsani123priya@users.noreply.github.com>
2018-07-30 10:41:14 +0530

    Paytm JDBC With Testcases

---
+++ PaytmDB/src/com/capgemini/core/beans/Wallet.java
@@ -0,0 +1,30 @@
+package com.capgemini.core.beans;
+import java.math.BigDecimal;
+
+public class Wallet {
+	
+	private BigDecimal balance;
+
+	
+	public Wallet() {
+		super();
+	}
+
+	public Wallet(BigDecimal amount) {
+		this.balance=amount;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return ", balance="+balance;
+	}
+
+}
---
+++ PaytmDB/src/com/capgemini/core/repo/WalletRepoImpl.java
@@ -0,0 +1,91 @@
+package com.capgemini.core.repo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.capgemini.core.beans.Customer;
+import com.capgemini.core.beans.Wallet;
+import com.capgemini.core.exception.InvalidInputException;
+import com.capgemini.core.util.DBUtil;
+
+public class WalletRepoImpl implements WalletRepo{
+
+	@Override
+	public boolean save(Customer customer) {
+		try(Connection con = DBUtil.getConnection())
+		{
+			PreparedStatement pstm = con.prepareStatement("insert into customers values(?,?,?)");
+			pstm.setString(1, customer.getName());
+			pstm.setString(2, customer.getMobileNo());
+			pstm.setBigDecimal(3, customer.getWallet().getBalance());
+			
+			pstm.execute();
+			return true;
+		} 
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public Customer findOne(String mobileNo) throws InvalidInputException {
+		try(Connection con = DBUtil.getConnection())
+		{
+			PreparedStatement pstm = con.prepareStatement("select * from customers where MOBILENO = ?");
+			pstm.setString(1, mobileNo);
+			ResultSet res = pstm.executeQuery();
+			
+			if(res.next() == false) throw new InvalidInputException("No customers with this mobile Number");
+			
+			Customer customer = new Customer();
+			Wallet wallet = new Wallet();
+			
+			customer.setName(res.getString(1));
+			customer.setMobileNo(res.getString(2));
+			
+			wallet.setBalance(res.getBigDecimal(3));
+			customer.setWallet(wallet);
+			
+			return customer;
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void remove(String mobileNo) {
+		try(Connection con = DBUtil.getConnection())
+		{
+			PreparedStatement pstm = con.prepareStatement("delete from customers where phone = ?");
+			
+			pstm.setString(1, mobileNo);
+			pstm.execute();
+			return;
+			
+		} 
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
